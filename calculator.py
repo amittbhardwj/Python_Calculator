69b394ae8abe178bb6e340f01a9022752357549a
Amitt Bhardwj <amitt.bhardwj@gmail.com>
2014-08-05 22:05:59 +0530

    Python Calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 # A Python Calculator
-from tkinter import *
+from Tkinter import *
+
 
 # the main class
 class Calc():
@@ -11,16 +12,36 @@ class Calc():
         self.op = ""
         self.eq_flag = False
 
-    def num_press(self,num):
+    def num_press(self, num):
         temp = text_box.get()
         self.eq_flag = False
         temp2 = str(num)
-        if self.new_num == True:
+        if self.new_num is True:
             self.current = temp2
             self.new_num = False
         else:
             if temp2 == '.':
                 if temp2 in temp:
-                    return self.current = temp + temp2
-        text_box.delete(0,END)
-        text_box.insert(0,self.current)
+                    return
+            self.current = temp + temp2
+        text_box.delete(0, END)
+        text_box.insert(0, self.current)
+
+    def calc_total(self):
+        if self.op_pending is True:
+            self.do_sum()
+            self.op_pending = False
+
+    def do_sum(self):
+        self.current = float(self.current)
+        if self.op is "add":
+            self.total += self.current
+        if self.op is "minus":
+            self.total -= self.current
+        if self.op is "times":
+            self.total *= self.current
+        if self.op is "divide":
+            self.total /= self.current
+        text_box.delete(0, END)
+        text_box.insert(0, self.total)
+        self.new_num = True
