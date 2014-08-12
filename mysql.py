73eea0a25e252a259f278d8ada782cf80643efa4
Amitt Bhardwj <amitt.bhardwj@gmail.com>
2014-08-13 00:57:57 +0530

    Python MYSQL connected

--- mysql.py
+++ mysql.py
@@ -2,15 +2,17 @@
 
 import MySQLdb
 
-value1 = raw_input("Enter First Value")
-value2 = raw_input("Enter Second Value")
+value1 = raw_input("Enter First Value: ")
+value2 = raw_input("Enter Second Value: ")
+value1 = int(value1)
+value2 = int(value2)
 
 if value1 > value2:
     greater = value1
 else:
     greater = value2
 
-print " The greater of two no. is " + greater
+print "The greater of two no. is ", greater
 
 # Open database connection
 db = MySQLdb.connect("localhost", "amit", "amitt", "cpp")
@@ -19,17 +21,17 @@ db = MySQLdb.connect("localhost", "amit", "amitt", "cpp")
 cur = db.cursor()
 
 # Prepare SQL query to INSERT a record into the database.
-sql = "INSERT INTO EMPLOYEE(Value1, Value2, Greater) \
+sql = "INSERT INTO PROGRAM(Value1, Value2, Greater) \
            VALUES ('%d', '%d', '%d' )" % \
-            (value1, value2, greater)
+           (value1, value2, greater)
 try:
     # Execute the SQL command
-          cur.execute(sql)
+        cur.execute(sql)
     # Commit your changes in the database
-          db.commit()
+        db.commit()
 except:
     # Rollback in case there is any error
-          db.rollback()
+        db.rollback()
 
 # disconnect from serve
 db.close()
